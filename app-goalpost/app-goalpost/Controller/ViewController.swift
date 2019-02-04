f1cf2c2b97ccb4e5604f6d929f4f7d4fa09e2a78
codyph <codyphilipp@gmail.com>
2019-02-04 19:00:33 +0800

    initial commit

---
+++ app-goalpost/app-goalpost/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-goalpost
+//
+//  Created by Bailey Aldridge on 4/2/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
