0e57437c1071d4729394b140a4912472e408ab51
codyph <codyphilipp@gmail.com>
2019-02-04 19:41:29 +0800

    edit2

---
+++ app-goalpost/app-goalpost/View/GoalCell.swift
@@ -0,0 +1,26 @@
+//
+//  GoalCell.swift
+//  app-goalpost
+//
+//  Created by Bailey Aldridge on 4/2/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    
+    // Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+        
+    }
+    
+}
