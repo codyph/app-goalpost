ffa71f2a09a99992017d1f424a7baaea01761fd7
codyph <codyphilipp@gmail.com>
2019-02-05 08:24:45 +0800

    edit4

--- app-goalpost/app-goalpost/Controller/GoalsVC.swift
+++ app-goalpost/app-goalpost/Controller/GoalsVC.swift
@@ -18,13 +18,46 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
     
     @IBAction func addGoalBtnTapped(_ sender: Any) {
     }
     
-
-
 }
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        
+        cell.configureCell(description: "Eat salad twice a day.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ app-goalpost/app-goalpost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  app-goalpost
+//
+//  Created by Bailey Aldridge on 5/2/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType : String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
